8ef2956d8e56ab06ea2d9ec4eee2b95381a378e2
kevin <kevin.aubel@epitech.eu>
2020-07-29 14:44:05 +0200

    add bootstrap

--- wordpress/wp-content/themes/party-organizer/functions.php
+++ wordpress/wp-content/themes/party-organizer/functions.php
@@ -1,3 +1,16 @@
 <?php
 
-add_theme_support('title-tag');
+// declare un function
+function party_support(){
+    add_theme_support('tittle-tag');
+};
+
+
+function party_register_assets(){
+    wp_register_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css');
+    wp_enqueue_style('bootstrap');
+}
+
+// lance une function a un evenement
+add_action('after_setup_theme', 'party_support');
+add_action('wp_enqueue_scripts', 'party_register_assets');
