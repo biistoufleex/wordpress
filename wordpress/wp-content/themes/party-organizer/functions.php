349029e7dcdbe34dc00010aedf73761375571ef2
kevin <kevin.aubel@epitech.eu>
2020-07-29 15:01:26 +0200

    gitignore

--- wordpress/wp-content/themes/party-organizer/functions.php
+++ wordpress/wp-content/themes/party-organizer/functions.php
@@ -12,6 +12,7 @@ function party_register_assets(){
     wp_register_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css');
     wp_register_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', ['popper', 'jquery']);
     wp_register_script('popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js');
+    wp_deregister_script('jquery');
     wp_register_script('jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n');
     wp_enqueue_style('bootstrap');
     wp_enqueue_script('bootstrap');
