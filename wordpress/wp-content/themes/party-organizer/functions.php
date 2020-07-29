43120d162fe1ae89578beb868977bd14b34a74b0
kevin <kevin.aubel@epitech.eu>
2020-07-29 14:51:19 +0200

    bootstrap + jquery

--- wordpress/wp-content/themes/party-organizer/functions.php
+++ wordpress/wp-content/themes/party-organizer/functions.php
@@ -8,7 +8,11 @@ function party_support(){
 
 function party_register_assets(){
     wp_register_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css');
+    wp_register_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', ['popper', 'jquery']);
+    wp_register_script('popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js');
+    wp_register_script('jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n');
     wp_enqueue_style('bootstrap');
+    wp_enqueue_script('bootstrap');
 }
 
 // lance une function a un evenement
