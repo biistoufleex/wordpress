a3753eb3d197305d984ad3e2b678f49636288c65
kevin <kevin.aubel@epitech.eu>
2020-07-29 14:57:26 +0200

    conflit

--- wordpress/wp-content/themes/party-organizer/functions.php
+++ wordpress/wp-content/themes/party-organizer/functions.php
@@ -20,19 +20,3 @@ function party_register_assets(){
 // lance une function a un evenement
 add_action('after_setup_theme', 'party_support');
 add_action('wp_enqueue_scripts', 'party_register_assets');
-
-
-function include_jquery()
-{
-    wp_deregister_script("jquery");
-    wp_enqueue_script( "jquery", get_template_directory_uri(). "/js/jquery-3.5.1.min.js", "", 1,true);
-    add_action("wp_enqueue_scripts","jquery");
-}
-add_action( "wp_enqueue_scripts","include_jquery");
-
-function loadjs()
-{
-    wp_register_script( "customjs", get_template_directory_uri(). "/js/scripts.js","", 1, true);
-    wp_enqueue_script("customjs");
-}
-add_action("wp_enqueue_scripts","loadjs");
